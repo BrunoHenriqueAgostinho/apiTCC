b75bcff2d5ab4f4e3e5e58279424302516a18cc8
Henrique Queiroz de Paula <henrique.q.paula@gmail.com>
2021-09-08 09:41:57 -0300

    deletar_usuario

--- api_academic/tb_usuario/deletar_usuario.php
+++ api_academic/tb_usuario/deletar_usuario.php
@@ -1,4 +1,9 @@
 <?php
+/*
+{
+    "cpf": "11111111111"
+}
+*/
 
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
@@ -8,15 +13,37 @@ if($_SERVER["REQUEST_METHOD"] == "GET"){
     $deco = json_decode($json);
     $cpf = $deco->cpf;
 
-    $sql = "DELETE FROM tb_usuario WHERE cpf_usuario = " . $cpf;
-    $resultado = mysqli_query($conexao, $sql);
-    if ($resultado) {
-        http_response_code(200);
-        $dados = ["mensagem" => "Usuário deletado com sucesso"];
-        echo json_encode($dados);
+    $sql1 = "SELECT 
+                *
+            FROM 
+                tb_usuario 
+            WHERE 
+                cpf_usuario = '$cpf'";
+    $resultado1 = mysqli_query($conexao, $sql1);
+    $contador = mysqli_num_rows($resultado1);
+    if($contador == 0){
+        header("HTTP/1.1 500 Registro inexistente.");
+        echo json_encode(["erro" => "Esse usuário não existe."]);
     } else {
-        header("HTTP/1.1 500 Erro no SQL");
-        echo json_encode(["erro" => "Erro SQL: " . $conexao->error]);
+
+        $permisaoDeletar = "SET foreign_key_checks = 0";
+        $resultado2 = mysqli_query($conexao, $permisaoDeletar);
+
+        $sql2 = "DELETE FROM 
+                    tb_usuario 
+                WHERE cpf_usuario = " . $cpf;
+        $resultado3 = mysqli_query($conexao, $sql2);
+        if ($resultado3) {
+            http_response_code(200);
+            $dados = ["mensagem" => "Usuário deletado com sucesso"];
+            echo json_encode($dados);
+        } else {
+            header("HTTP/1.1 500 Erro no SQL");
+            echo json_encode(["erro" => "Erro SQL: " . $conexao->error]);
+        }
+
+        $habilitarDeletar = "SET foreign_key_checks = 1";
+        $resultado4 = mysqli_query($conexao, $habilitarDeletar);
     }
 }
 ?>
