0b127f32752face1c13c1526582a377100947f39
jonathan500 <jonathanricardopizidoro@gmail.com>
2021-09-13 12:41:31 -0300

    verificação de cnpj e cpf

--- api_academic/tb_instituicao/inserirCad_instituicao.php
+++ api_academic/tb_instituicao/inserirCad_instituicao.php
@@ -13,6 +13,7 @@ header("Content-Type: application/json");
 header("Access-Controlo-Allow-Origin: *");
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     require("../conexao.php");
+    require("../validarCnpj.php");
     $json = file_get_contents("php://input");
     $deco = json_decode($json);
     $cnpj = $deco->cnpj;
@@ -21,63 +22,67 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     $email = $deco->email;
     //$timezone = new DateTimeZone('America/Sao_Paulo');
     $dtCadastro = date('Y-m-d');//new date('now', $timezone);
-
-    $sql1 = "SELECT
+    if(validar_cnpj($cnpj)){
+        $sql1 = "SELECT
+                    *
+                FROM
+                    tb_instituicao
+                WHERE
+                    cnpj_instituicao = '$cnpj'";
+        $resultado1 = mysqli_query($conexao, $sql1);
+        $contador1 = mysqli_num_rows($resultado1);
+        if ($contador1 == 0) {
+            $sql2 = "SELECT 
                 *
-            FROM
-                tb_instituicao
-            WHERE
-                cnpj_instituicao = '$cnpj'";
-    $resultado1 = mysqli_query($conexao, $sql1);
-    $contador1 = mysqli_num_rows($resultado1);
-    if ($contador1 == 0) {
-        $sql2 = "SELECT 
-            *
-        FROM 
-            tb_contato 
-        WHERE 
-            email_contato = '$email'";
-            $resultado2 = mysqli_query($conexao, $sql2);
-            $contador2 = mysqli_num_rows($resultado2);
-            if($contador2 == 0){
-                $sql3 = "INSERT INTO tb_contato (email_contato) VALUES
-                            ('$email')";
-                $resultado3 = mysqli_query($conexao, $sql3);
-                if($resultado3){
-                    $sql4 = "SELECT codigo_contato
-                            FROM    
-                                tb_contato
-                            WHERE
-                                email_contato LIKE '$email'";
-                    $resultado4 = mysqli_query($conexao, $sql4);
-                    $dados = mysqli_fetch_array($resultado4);
-                    $contato = $dados["codigo_contato"];
-                    if($resultado4){
-                        $sql5 = "INSERT INTO tb_instituicao(cnpj_instituicao, nome_instituicao, senha_instituicao, dtCadastro_instituicao, Tb_Contato_codigo_contato) VALUES
-                        ('$cnpj', '$nome', '$senha', '$dtCadastro', '$contato')";
-                        $resultado5 = mysqli_query($conexao,$sql5);
-                        if($resultado5){
-                            http_response_code(201);
-                            echo json_encode(["mensagem" => "Instituição cadastrada com sucesso."]);
+            FROM 
+                tb_contato 
+            WHERE 
+                email_contato = '$email'";
+                $resultado2 = mysqli_query($conexao, $sql2);
+                $contador2 = mysqli_num_rows($resultado2);
+                if($contador2 == 0){
+                    $sql3 = "INSERT INTO tb_contato (email_contato) VALUES
+                                ('$email')";
+                    $resultado3 = mysqli_query($conexao, $sql3);
+                    if($resultado3){
+                        $sql4 = "SELECT codigo_contato
+                                FROM    
+                                    tb_contato
+                                WHERE
+                                    email_contato LIKE '$email'";
+                        $resultado4 = mysqli_query($conexao, $sql4);
+                        $dados = mysqli_fetch_array($resultado4);
+                        $contato = $dados["codigo_contato"];
+                        if($resultado4){
+                            $sql5 = "INSERT INTO tb_instituicao(cnpj_instituicao, nome_instituicao, senha_instituicao, dtCadastro_instituicao, Tb_Contato_codigo_contato) VALUES
+                            ('$cnpj', '$nome', '$senha', '$dtCadastro', '$contato')";
+                            $resultado5 = mysqli_query($conexao,$sql5);
+                            if($resultado5){
+                                http_response_code(201);
+                                echo json_encode(["mensagem" => "Instituição cadastrada com sucesso."]);
+                            }else{
+                                header("HTTP/1.1 500 Erro no SQL");
+                                echo json_encode(["erro" => "Erro ao cadastrar instituição."]);
+                            }
                         }else{
-                            header("HTTP/1.1 500 Erro no SQL");
-                            echo json_encode(["erro" => "Erro ao cadastrar instituição."]);
+                            header("HTTP/1.1 500 Erro de inserção");
+                            echo json_encode(["erro" => "Erro ao encontrar o contato da instituição."]); 
                         }
                     }else{
                         header("HTTP/1.1 500 Erro de inserção");
-                        echo json_encode(["erro" => "Erro ao encontrar o contato da instituição."]); 
+                        echo json_encode(["erro" => "Erro ao cadastrar o email."]);
                     }
                 }else{
-                    header("HTTP/1.1 500 Erro de inserção");
-                    echo json_encode(["erro" => "Erro ao cadastrar o email."]);
+                    header("HTTP/1.1 500 Registro já existente");
+                    echo json_encode(["erro" => "Esse email já está sendo utilizado por outra pessoa."]);
                 }
-            }else{
-                header("HTTP/1.1 500 Registro já existente");
-                echo json_encode(["erro" => "Esse email já está sendo utilizado por outra pessoa."]);
-            }
-    } else {
+        } else {
+            header("HTTP/1.1 500 Registro já existente");
+            echo json_encode(["erro" => "Esse CNPJ já está sendo utilizado."]);
+        }
+    }else{
         header("HTTP/1.1 500 Registro já existente");
-        echo json_encode(["erro" => "Esse CNPJ já está sendo utilizado."]);
+        echo json_encode(["erro" => "Para CNPJ são somente permitidos números e até 14 caracteres"]);
     }
 }
 ?>
--- api_academic/tb_usuario/inserirCad_usuario.php
+++ api_academic/tb_usuario/inserirCad_usuario.php
@@ -12,6 +12,7 @@ header("Content-Type: application/json");
 //header("Access-Controlo-Allow-Origin: *");
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     require("../conexao.php");
+    require("../validaCpf.php");
     $json = file_get_contents("php://input");
     $deco = json_decode($json);
     $cpf = $deco->cpf;
@@ -19,67 +20,71 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     $senha = $deco->senha;
     $email = $deco->email;
     $dtCadastro = date('Y-m-d');
-    
-    $sql1 = "SELECT 
-                *
-            FROM 
-                tb_usuario 
-            WHERE 
-                cpf_usuario = '$cpf'";
-    $resultado1 = mysqli_query($conexao, $sql1);
-    $contador = mysqli_num_rows($resultado1);
-    if ($contador == 0){
-        $sql2 = "SELECT 
-                *
-            FROM 
-                tb_contato 
-            WHERE 
-                email_contato = '$email'";
+    if(validaCPF($cpf)){
+        $sql1 = "SELECT 
+                    *
+                FROM 
+                    tb_usuario 
+                WHERE 
+                    cpf_usuario = '$cpf'";
+        $resultado1 = mysqli_query($conexao, $sql1);
+        $contador = mysqli_num_rows($resultado1);
+        if ($contador == 0){
+            $sql2 = "SELECT 
+                    *
+                FROM 
+                    tb_contato 
+                WHERE 
+                    email_contato = '$email'";
 
-        $resultado2 = mysqli_query($conexao, $sql2);
-        $contador2 = mysqli_num_rows($resultado2);
+            $resultado2 = mysqli_query($conexao, $sql2);
+            $contador2 = mysqli_num_rows($resultado2);
 
-        if($contador2 == 0){
-            $sql3 = "INSERT INTO tb_contato (email_contato) VALUES 
-                        ('$email')";
-            $resultado3 = mysqli_query($conexao, $sql3);
-            if($resultado3){
-                $sql4 = "SELECT 
-                            codigo_contato
-                        FROM 
-                            tb_contato
-                        WHERE
-                            email_contato LIKE '$email'";
-                $resultado4 = mysqli_query($conexao, $sql4);
+            if($contador2 == 0){
+                $sql3 = "INSERT INTO tb_contato (email_contato) VALUES 
+                            ('$email')";
+                $resultado3 = mysqli_query($conexao, $sql3);
+                if($resultado3){
+                    $sql4 = "SELECT 
+                                codigo_contato
+                            FROM 
+                                tb_contato
+                            WHERE
+                                email_contato LIKE '$email'";
+                    $resultado4 = mysqli_query($conexao, $sql4);
 
-                if($resultado4){
-                    $dados = mysqli_fetch_array($resultado4);
-                    $contato = $dados["codigo_contato"];
-                    $sql5 = "INSERT INTO tb_usuario (cpf_usuario, nome_usuario, senha_usuario, dtCadastro_usuario, Tb_Contato_codigo_contato) VALUES
-                                ('$cpf', '$nome', '$senha', '$dtCadastro', '$contato')";
-                    $resultado5 = mysqli_query($conexao, $sql5);
-                    if($resultado5){
-                        http_response_code(201);
-                        echo json_encode(["mensagem" => "Usuário cadastrado com sucesso."]);
+                    if($resultado4){
+                        $dados = mysqli_fetch_array($resultado4);
+                        $contato = $dados["codigo_contato"];
+                        $sql5 = "INSERT INTO tb_usuario (cpf_usuario, nome_usuario, senha_usuario, dtCadastro_usuario, Tb_Contato_codigo_contato) VALUES
+                                    ('$cpf', '$nome', '$senha', '$dtCadastro', '$contato')";
+                        $resultado5 = mysqli_query($conexao, $sql5);
+                        if($resultado5){
+                            http_response_code(201);
+                            echo json_encode(["mensagem" => "Usuário cadastrado com sucesso."]);
+                        }else{
+                            header("HTTP/1.1 500 Erro no SQL");
+                            echo json_encode(["erro" => "Erro ao cadastrar usuário."]);
+                        }
                     }else{
-                        header("HTTP/1.1 500 Erro no SQL");
-                        echo json_encode(["erro" => "Erro ao cadastrar usuário."]);
+                        header("HTTP/1.1 500 Erro de inserção");
+                        echo json_encode(["erro" => "Erro ao encontrar o contato do usuario."]); 
                     }
                 }else{
                     header("HTTP/1.1 500 Erro de inserção");
-                    echo json_encode(["erro" => "Erro ao encontrar o contato do usuario."]); 
+                    echo json_encode(["erro" => "Erro ao cadastrar o email."]);
                 }
             }else{
-                header("HTTP/1.1 500 Erro de inserção");
-                echo json_encode(["erro" => "Erro ao cadastrar o email."]);
+                header("HTTP/1.1 500 Registro já existente");
+                echo json_encode(["erro" => "Esse email já está sendo utilizado por outra pessoa."]);
             }
-        }else{
+        } else {
             header("HTTP/1.1 500 Registro já existente");
-            echo json_encode(["erro" => "Esse email já está sendo utilizado por outra pessoa."]);
+            echo json_encode(["erro" => "Esse CPF já está sendo utilizado."]);
         }
-    } else {
+    }else{
         header("HTTP/1.1 500 Registro já existente");
-        echo json_encode(["erro" => "Esse CPF já está sendo utilizado."]);
+        echo json_encode(["erro" => "CPF inexistente"]);
     }
 }
 ?>
