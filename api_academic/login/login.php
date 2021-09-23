f8c33de9f9894673da3e705079fa845630f64e39
BrunoHenriqueAgostinho <bruno.silva1847@etec.sp.gov.br>
2021-09-23 18:40:55 -0300

    23-09-2021 Atualização da API login

--- api_academic/login/login.php
+++ api_academic/login/login.php
@@ -1,42 +1,49 @@
 <?php
 header("Content-Type: application/json");
-//header("Access-Controlo-Allow-Origin: *");
-if($_SERVER["REQUEST_METHOD"] == "GET"){
-    require("../conexao.php");
+header("Access-Controlo-Allow-Origin: *");
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $conexao = new PDO("mysql:host=localhost:3306;dbname=academic2", 'root', '');
     $json = file_get_contents("php://input");
     $deco = json_decode($json);
     $email = $deco->email;
     $senha_des = $deco->senha;
     $senha = md5($senha_des);
     
-    $sql1 = "SELECT codigo_contato FROM tb_contato WHERE email_contato LIKE '$email'";
-    $resultado1 = mysqli_query($conexao, $sql1);
-    $contador = mysqli_num_rows($resultado1);
-    if($contador==0){
-        echo json_encode(["login" => "0"]);
+    $sql1 = $conexao->prepare("SELECT codigo_contato FROM tb_contato WHERE email_contato LIKE :email");
+    $sql1->bindValue(':email', $email, PDO::PARAM_STR);
+    $sql1->execute();
+    $resultado1 = $sql1->fetchAll(PDO::FETCH_ASSOC);
+    $contador1 = count($resultado1);
+    if($contador1 == 0){
+        header("HTTP/1.1 500 Erro no SQL");
+        echo json_encode(["erro" => "Registro não existente."]);
     }else{
-        $dados = mysqli_fetch_array($resultado1);
-        $contato = $dados["codigo_contato"];
-        $sql2 = "SELECT cnpj_instituicao FROM tb_instituicao WHERE Tb_Contato_codigo_contato = $contato AND senha_instituicao LIKE '$senha'";
-        $resultado2 = mysqli_query($conexao, $sql2);
-        $contador2 = mysqli_num_rows($resultado2);
+        $contato = $resultado1["0"]["codigo_contato"];
+        $sql2 = $conexao->prepare("SELECT cnpj_instituicao FROM tb_instituicao WHERE Tb_Contato_codigo_contato = :contato AND senha_instituicao LIKE :senha");
+        $sql2->bindValue(':contato', $contato, PDO::PARAM_INT);
+        $sql2->bindValue(':senha', $senha, PDO::PARAM_STR);
+        $sql2->execute();
+        $resultado2 = $sql2->fetchAll(PDO::FETCH_ASSOC);
+        $contador2 = count($resultado2);
         if($contador2 == 0){
-            $sql3 = "SELECT cpf_usuario FROM tb_usuario WHERE Tb_Contato_codigo_contato = $contato AND senha_usuario LIKE '$senha'";
-            $resultado3 = mysqli_query($conexao, $sql3);
-            $contador3 = mysqli_num_rows($resultado3);
+            $sql3 = $conexao->prepare("SELECT cpf_usuario FROM tb_usuario WHERE Tb_Contato_codigo_contato = :contato AND senha_usuario LIKE :senha");
+            $sql3->bindValue(':contato', $contato, PDO::PARAM_INT);
+            $sql3->bindValue(':senha', $senha, PDO::PARAM_STR);
+            $sql3->execute();
+            $resultado3 = $sql3->fetchAll(PDO::FETCH_ASSOC);
+            $contador3 = count($resultado3);
             if($contador3 == 0){
-                echo json_encode(["login" => "1"]);
+                header("HTTP/1.1 500 Erro no SQL");
+                echo json_encode(["erro" => "Senha incorreta."]);
             }else{
-                $dados = mysqli_fetch_array($resultado3);
-                $usuario = $dados["cpf_usuario"];
+                $usuario = $resultado3["0"]["cpf_usuario"];
                 http_response_code(200);
-                echo json_encode(["cpf"=>"$usuario"]);
+                echo json_encode(["codigo"=>"$usuario"]);
             }
         }else{
-            $dados = mysqli_fetch_array($resultado2);
-            $instituicao = $dados["cnpj_instituicao"];
+            $instituicao = $resultado2["0"]["cnpj_instituicao"];
             http_response_code(200);
-            echo json_encode(["cnpj"=>"$instituicao"]);
+            echo json_encode(["codigo"=>"$instituicao"]);
         }
 
     }
