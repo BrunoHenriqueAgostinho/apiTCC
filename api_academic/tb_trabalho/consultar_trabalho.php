0029dc8f9ec374c297c2a555c4328b9ace8ddfb9
BrunoHenriqueAgostinho <bruno.silva1847@etec.sp.gov.br>
2021-10-13 12:50:01 -0300

    13-10-2021 Criação do arquivo consultar_trabalho

---
+++ api_academic/tb_trabalho/consultar_trabalho.php
@@ -0,0 +1,37 @@
+<?php
+header("Content-Type: application/json");
+//header("Access-Control-Allow-Origin: *");
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    require("../conexao.php");
+    $json = file_get_contents("php://input");
+    $deco = json_decode($json);
+    $codigo = $deco->codigo;
+
+    $sql = "SELECT 
+                codigo_trabalho as codigo,
+                nome_trabalho as nome,
+                descricao_trabalho as descricao,
+                arquivo_trabalho as arquivo,
+                formatacao_trabalho as formatacao,
+                finalizado_trabalho as finalizado,
+                dtCriacao_trabalho as dtCriacao,
+                dtAlteracao_trabalho as dtAlteracao,
+                dtPublicacao_trabalho as dtPublicacao,
+                avaliacao_trabalho as avaliacao,
+                Tb_Modelo_codigo_modelo as modelo,
+                Tb_instituicao_cnpj_instituicao as cnpj
+            FROM 
+                tb_trabalho
+            WHERE 
+                codigo_trabalho = $codigo";
+    $resultado = mysqli_query($conexao, $sql);
+    $contador = mysqli_num_rows($resultado);
+    if ($contador == 0) {
+        header("HTTP/1.1 500 Erro no SQL");
+        echo json_encode(["erro" => "Erro ao consultar usuário."]);
+    } else {
+        $dados = $resultado->fetch_array(MYSQLI_ASSOC);
+        echo json_encode($dados, JSON_UNESCAPED_UNICODE);
+    }
+}
+?>
