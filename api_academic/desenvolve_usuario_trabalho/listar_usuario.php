f928374a0997a2905b9a00e54388c212a69f5256
BrunoHenriqueAgostinho <bruno.silva1847@etec.sp.gov.br>
2021-10-19 15:07:13 -0300

    19-10-2021 Criação de API para exibir membros de um trabalho

---
+++ api_academic/desenvolve_usuario_trabalho/listar_usuario.php
@@ -0,0 +1,41 @@
+<?php
+
+header("Content-Type: application/json");
+//header("Access-Control-Allow-Origin: *");
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    require("../conexao.php");
+    $json = file_get_contents("php://input");
+    $deco = json_decode($json);
+    $codigo = $deco->codigo;
+
+    $sql = "SELECT 
+                U.cpf_usuario as cpf,
+                U.nome_usuario as nome,
+                U.senha_usuario as senha,
+                U.descricao_usuario as descricao,
+                U.foto_usuario as foto,
+                U.dtCadastro_usuario as dtCadastro,
+                U.tema_usuario as tema,
+                U.status_usuario as status,
+                U.contaStatus_usuario as contaStatus,
+                U.email_usuario as email,
+                U.telefoneFixo_usuario as telefoneFixo,
+                U.telefoneCelular_usuario as telefoneCelular 
+            FROM
+                tb_usuario U, desenvolve_usuario_trabalho D
+            WHERE
+            D.Tb_Trabalho_codigo_trabalho = $codigo AND U.cpf_usuario = D.Tb_Usuario_cpf_usuario";
+                //D.Tb_Trabalho_codigo_trabalho = $codigo AND U.cpf_usuario = D.Tb_Usuario_cpf_usuario
+    $resultado = mysqli_query($conexao, $sql);
+    $contador = mysqli_num_rows($resultado);
+    if ($contador == 0){
+        header("HTTP/1.1 500 Erro ao consultar banco");
+        echo json_encode(["erro" => "Não foi possível consultar os membros deste trabalho."]);
+    } else {
+        $dados = $resultado->fetch_all(MYSQLI_ASSOC);
+        http_response_code(200);
+        echo json_encode($dados, JSON_UNESCAPED_UNICODE);
+    }
+}
+
+?>
