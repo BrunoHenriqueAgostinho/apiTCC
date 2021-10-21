42d9e4c49ea1aa2669adf10f7119d4cbe3fbe4cf
Etec Antonio Devisate <etec@devisate.com.br>
2021-10-21 13:12:58 -0300

    21-20-2021 Modificações da homepage-system e meustrabalhos-system

---
+++ api_academic/tb_trabalho/pesquisar_trabalho.php
@@ -0,0 +1,44 @@
+<?php
+/*{
+	"pesquisa": "B"
+}*/
+
+header("Content-Type: application/json");
+//header("Access-Control-Allow-Origin: *");
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    require("../conexao.php");
+    $json = file_get_contents("php://input");
+    $deco = json_decode($json);
+    $pesquisa = $deco->pesquisa;
+
+    if($pesquisa != ''){
+        $sql1 = "SELECT 
+            codigo_trabalho as codigo,
+            nome_trabalho as nome,
+            descricao_trabalho as descricao,
+            arquivo_trabalho as arquivo,
+            formatacao_trabalho as formatacao,
+            finalizado_trabalho as finalizado,
+            dtCriacao_trabalho as dtCriacao,
+            dtAlteracao_trabalho as dtAlteracao,
+            dtPublicacao_trabalho as dtPublicacao,
+            avaliacao_trabalho as avaliacao,
+            Tb_Modelo_codigo_modelo as modelo,
+            Tb_instituicao_cnpj_instituicao as cnpj
+        FROM 
+            tb_trabalho
+        WHERE 
+            finalizado_trabalho = 1 AND nome_trabalho LIKE '%$pesquisa%'";
+
+        $resultado1 = mysqli_query($conexao, $sql1); 
+        if (mysqli_num_rows($resultado1) >= 1) { 
+            $dados = $resultado1->fetch_all(MYSQLI_ASSOC);
+            http_response_code(200);
+            echo json_encode($dados, JSON_UNESCAPED_UNICODE);
+        } else {
+            header("HTTP/1.1 500 Erro no SQL");
+            echo json_encode(["erro" => "Nenhum resultado encontrado."]);
+        }
+    }
+}
+?>
