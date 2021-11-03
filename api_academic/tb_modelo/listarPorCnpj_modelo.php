d805d20136cce550c617d14cd7aa02478c032c8e
Etec Antonio Devisate <etec@devisate.com.br>
2021-11-03 08:48:33 -0300

    2021-11-03 Início modificações para permitir funções das instituições

--- api_academic/tb_modelo/listarPorCnpj_modelo.php
+++ api_academic/tb_modelo/listarPorCnpj_modelo.php
@@ -6,28 +6,26 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     require("../conexao.php");
     $json = file_get_contents("php://input");
     $deco = json_decode($json);
-    $pesquisa = $deco->pesquisa;
+    $cnpj = $deco->cnpj;
 
     $sql1 = "SELECT 
                 * 
             FROM 
                 tb_instituicao 
             WHERE 
-                nome_instituicao like '%$pesquisa%'";
+            Tb_Instituicao_cnpj_instituicao = '$cnpj'";
     $resultado1 = mysqli_query($conexao, $sql1);
     $contador1 = mysqli_num_rows($resultado1);
     if ($contador1 == 0){
         header("HTTP/1.1 500 Registro inexistente.");
         echo json_encode(["erro" => "Não encontramos essa instituição."]);
     } else {
-        $valores = mysqli_fetch_array($resultado1);
-        $codigo = $valores["cnpj_instituicao"];
         $sql2 = "SELECT 
                     codigo_modelo as codigo, nome_modelo as nome, arquivo_modelo as arquivo, margemDireita_modelo as margemDireita, margemEsquerda_modelo as margemEsquerda, margemTopo_modelo as margemTopo, margemBaixo_modelo as margemBaixo, dtCriacao_modelo as dtCriacao, descricao_modelo as descricao, Tb_instituicao_cnpj_instituicao as cnpj 
                 FROM 
                     tb_modelo 
                 WHERE
-                    Tb_Instituicao_cnpj_instituicao = '$codigo'";
+                    Tb_Instituicao_cnpj_instituicao = '$cnpj'";
         $resultado2 = mysqli_query($conexao, $sql2);
         if ($resultado2) {
             $dados = $resultado2->fetch_all(MYSQLI_ASSOC);
