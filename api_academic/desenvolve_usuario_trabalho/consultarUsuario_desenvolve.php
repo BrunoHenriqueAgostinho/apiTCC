5009e7ac6c47430f8bcef2dfa238910176a2409e
BrunoHenriqueAgostinho <bruno.silva1847@etec.sp.gov.br>
2021-11-04 17:22:13 -0300

    2021-11-04 Criação de APIs para permitir funções das instituições

---
+++ api_academic/desenvolve_usuario_trabalho/consultarUsuario_desenvolve.php
@@ -0,0 +1,30 @@
+<?php
+
+header("Content-Type: application/json");
+//header("Access-Control-Allow-Origin: *");
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    require("../conexao.php");
+    $json = file_get_contents("php://input");
+    $deco = json_decode($json);
+    $cpf = $deco->cpf;
+    $codigo = $deco->codigo;
+
+    $sql = "SELECT 
+                count(Tb_Usuario_cpf_usuario) as usuario
+            FROM
+                desenvolve_usuario_trabalho
+            WHERE
+                Tb_Usuario_cpf_usuario = $cpf AND Tb_Trabalho_codigo_trabalho = $codigo";
+    $resultado = mysqli_query($conexao, $sql);
+    $contador = mysqli_num_rows($resultado);
+    if ($contador == 0){
+        header("HTTP/1.1 500 Erro ao consultar banco");
+        echo json_encode(["erro" => "Não foi possível consultar os membros deste trabalho."]);
+    } else {
+        $dados = $resultado->fetch_array(MYSQLI_ASSOC);
+        http_response_code(200);
+        echo json_encode($dados, JSON_UNESCAPED_UNICODE);
+    }
+}
+
+?>
