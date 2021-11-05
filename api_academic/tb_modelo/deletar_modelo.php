b23e2a24e5b8870cd4a1144e4b06a1feaef3786b
BrunoHenriqueAgostinho <bruno.silva1847@etec.sp.gov.br>
2021-11-05 17:56:17 -0300

    2021-11-05 Função de deletar modelo

--- api_academic/tb_modelo/deletar_modelo.php
+++ api_academic/tb_modelo/deletar_modelo.php
@@ -5,24 +5,21 @@
 
 header("Content-Type: application/json");
 //header("Access-Control-Allow-Origin: *");
-if($_SERVER["REQUEST_METHOD"] == "GET"){
+if($_SERVER["REQUEST_METHOD"] == "POST"){
     require("../conexao.php");
     $json = file_get_contents("php://input");
     $deco = json_decode($json);
     $codigo = $deco->codigo;
-    
-    $sql1 = "SELECT
-                *
-            FROM
-                tb_modelo
-            WHERE
-                codigo_modelo = $codigo";
+
+    $sql1 = "UPDATE 
+                tb_trabalho 
+            SET 
+                Tb_Modelo_codigo_modelo = 1 
+            WHERE 
+                Tb_Modelo_codigo_modelo = $codigo";
     $resultado1 = mysqli_query($conexao, $sql1);
-    $contador1 = mysqli_num_rows($resultado1);
-    if ($contador1 == 0){
-        header("HTTP/1.1 500 Registro inexistente.");
-        echo json_encode(["erro" => "Esse modelo não existe."]);
-    } else {
+
+    if($resultado1) {
         $sql2 = "DELETE FROM tb_modelo WHERE codigo_modelo = $codigo";
         $resultado2 = mysqli_query($conexao, $sql2);
 
@@ -32,8 +29,12 @@ if($_SERVER["REQUEST_METHOD"] == "GET"){
             echo json_encode($dados);
         } else {
             header("HTTP/1.1 500 Erro no SQL");
-            echo json_encode(["erro" => "Erro ao deletar modelo."]);
+            echo json_encode(["erro" => mysqli_error($conexao)]);
+            //"Erro ao deletar modelo."
         }
+    } else {
+        header("HTTP/1.1 500 Registro inexistente.");
+        echo json_encode(["erro" => "Esse modelo não existe."]);
     }
 }
 ?>
--- api_academic/tb_modelo/listar_modelo.php
+++ api_academic/tb_modelo/listar_modelo.php
@@ -8,37 +8,32 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     $deco = json_decode($json);
     $pesquisa = $deco->pesquisa;
 
-    $sql1 = "SELECT 
-                * 
-            FROM 
-                tb_instituicao 
-            WHERE 
-                nome_instituicao like '%$pesquisa%'";
+    $sql1 = "SELECT * FROM tb_instituicao WHERE nome_instituicao like '%$pesquisa%'";
     $resultado1 = mysqli_query($conexao, $sql1);
     $contador1 = mysqli_num_rows($resultado1);
+
     if ($contador1 == 0){
         header("HTTP/1.1 500 Registro inexistente.");
         echo json_encode(["erro" => "Não encontramos essa instituição."]);
     } else {
-        $valores = mysqli_fetch_array($resultado1);
-        $codigo = $valores["cnpj_instituicao"];
-        $sql2 = "SELECT 
-                    codigo_modelo as codigo, nome_modelo as nome, arquivo_modelo as arquivo, margemDireita_modelo as margemDireita, margemEsquerda_modelo as margemEsquerda, margemTopo_modelo as margemTopo, margemBaixo_modelo as margemBaixo, dtCriacao_modelo as dtCriacao, descricao_modelo as descricao, Tb_instituicao_cnpj_instituicao as cnpj 
+        $dados1 = mysqli_fetch_array($resultado1);
+        $codigo = $dados1["cnpj_instituicao"];
+
+        $sql2 = "SELECT codigo_modelo as codigo, nome_modelo as nome, arquivo_modelo as arquivo, margemDireita_modelo as margemDireita, margemEsquerda_modelo as margemEsquerda, margemTopo_modelo as margemTopo, margemBaixo_modelo as margemBaixo, dtCriacao_modelo as dtCriacao, descricao_modelo as descricao, Tb_instituicao_cnpj_instituicao as cnpj 
                 FROM 
                     tb_modelo 
                 WHERE
-                    Tb_Instituicao_cnpj_instituicao = '$codigo'";
+                Tb_Instituicao_cnpj_instituicao LIKE '%$codigo%'";
         $resultado2 = mysqli_query($conexao, $sql2);
         if ($resultado2) {
-            $dados = $resultado2->fetch_all(MYSQLI_ASSOC);
+            $dados2 = $resultado2->fetch_all(MYSQLI_ASSOC);
             http_response_code(200);
-            echo json_encode($dados, JSON_UNESCAPED_UNICODE);
+            echo json_encode($dados2, JSON_UNESCAPED_UNICODE);
         } else {
             header("HTTP/1.1 500 Erro no SQL");
             echo json_encode(["erro" => "Erro ao listar modelos."]);
         }
+        
     }
-
-
 } 
 ?>
