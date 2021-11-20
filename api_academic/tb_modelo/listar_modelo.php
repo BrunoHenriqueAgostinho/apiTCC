1ead39fbef8afb5bd53e458a00a6032719c8a7a8
BrunoHenriqueAgostinho <bruno.silva1847@etec.sp.gov.br>
2021-11-20 09:00:19 -0300

    2021-11-20 Modificação na listagem de modelos

--- api_academic/tb_modelo/listar_modelo.php
+++ api_academic/tb_modelo/listar_modelo.php
@@ -13,14 +13,37 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     $contador1 = mysqli_num_rows($resultado1);
 
     if ($contador1 == 0){
-        header("HTTP/1.1 500 Registro inexistente.");
-        //Não encontramos essa instituição
-        echo json_encode(["erro" => "Houve um problema ao fazer a listagem de modelos"]);
+        $sql2 = "SELECT 
+                    codigo_modelo as codigo, 
+                    nome_modelo as nome, 
+                    arquivo_modelo as arquivo, 
+                    margemDireita_modelo as margemDireita, 
+                    margemEsquerda_modelo as margemEsquerda, 
+                    margemTopo_modelo as margemTopo, 
+                    margemBaixo_modelo as margemBaixo, 
+                    dtCriacao_modelo as dtCriacao, 
+                    descricao_modelo as descricao, 
+                    Tb_instituicao_cnpj_instituicao as cnpj 
+                FROM 
+                    Tb_Modelo 
+                WHERE
+                    Tb_Instituicao_cnpj_instituicao = null";
+                    
+        $resultado2 = mysqli_query($conexao, $sql2);
+        if ($resultado2) {
+            $dados2 = $resultado2->fetch_all(MYSQLI_ASSOC);
+            http_response_code(200);
+            echo json_encode($dados2, JSON_UNESCAPED_UNICODE);
+        } else {
+            header("HTTP/1.1 500 Erro no SQL");
+            //Erro ao listar modelos
+            echo json_encode(["erro" => "Houve um problema ao fazer a listagem de modelos"]);
+        }
     } else {
         $dados1 = mysqli_fetch_array($resultado1);
         $codigo = $dados1["cnpj_instituicao"];
 
-        $sql2 = "SELECT 
+        $sql3 = "SELECT 
                     codigo_modelo as codigo, 
                     nome_modelo as nome, 
                     arquivo_modelo as arquivo, 
@@ -36,17 +59,16 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 WHERE
                     Tb_Instituicao_cnpj_instituicao LIKE '%$codigo%'";
                     
-        $resultado2 = mysqli_query($conexao, $sql2);
-        if ($resultado2) {
-            $dados2 = $resultado2->fetch_all(MYSQLI_ASSOC);
+        $resultado3 = mysqli_query($conexao, $sql3);
+        if ($resultado3) {
+            $dados3 = $resultado3->fetch_all(MYSQLI_ASSOC);
             http_response_code(200);
-            echo json_encode($dados2, JSON_UNESCAPED_UNICODE);
+            echo json_encode($dados3, JSON_UNESCAPED_UNICODE);
         } else {
             header("HTTP/1.1 500 Erro no SQL");
             //Erro ao listar modelos
             echo json_encode(["erro" => "Houve um problema ao fazer a listagem de modelos"]);
         }
-        
     }
 } 
 ?>
