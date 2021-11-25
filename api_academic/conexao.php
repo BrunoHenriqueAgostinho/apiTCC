8af8a2146cb6a92741dac77c007adcec6b6b75de
BrunoHenriqueAgostinho <henrique.q.paula@gmail.com>
2021-11-25 09:21:47 -0200

    ajustes api

--- api_academic/conexao.php
+++ api_academic/conexao.php
@@ -2,9 +2,9 @@
     //academicoetec.mysql.dbaas.com.br caso o ip nao esteja funcionando
     // PARA USAR A CONEXAO COM O SERVIDOR UTILIZE DESCOMENTE ESSA PARTE
     // host far from  home
-    $host = "201.62.65.6"; 
+    //$host = "201.62.65.6"; 
     // host local na escola
-    // $host = "172.16.20.100"; 
+    $host = "172.16.20.100"; 
     
     $user = "academic";
     $password = "Academic@2021";
@@ -19,13 +19,13 @@
     // $dsn = "mysql:host={$host};port=3306;dbname={$base}";
     
     //NÃO COMENTAR ESTA PARTE SE NÃO, NÃO IRA FUNCIONAR
-    $conexao = @mysqli_connect($host, $user, $password);
+    $conexao = new mysqli($host, $user, $password);
     $conexao->set_charset("UTF8");
     try{
         $conexao2 = new PDO($dsn, $user, $password);
     }catch (PDOException $e){
         die($e->getMessage());
-    }   
+    }
     if ($conexao->connect_error) {
         die("Falha ao conectar: " . $conexao->connect_error);
     }
